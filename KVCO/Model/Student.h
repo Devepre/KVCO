614456e9ab942ded7bdc4c42e1249284c65d4bae
Devepre <bicycle2life@gmail.com>
2018-04-10 13:37:07 +0300

    added gender for model

--- KVCO/Model/Student.h
+++ KVCO/Model/Student.h
@@ -1,10 +1,17 @@
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, Gender) {
+    GenderMale,
+    GenderFemale,
+    GenderOther,
+};
+
 @interface Student : NSObject
 
 @property (strong, nonatomic) NSString      *name;
 @property (strong, nonatomic) NSString      *surname;
 @property (strong, nonatomic) NSDate        *dateOfBirth;
+@property (assign, nonatomic) Gender        gender;
 @property (assign, nonatomic) NSInteger     grade;
 
 - (instancetype)initWithName:(NSString *)name andSurname:(NSString *)surname andBirthDate:(NSDate *)birthDate NS_DESIGNATED_INITIALIZER;

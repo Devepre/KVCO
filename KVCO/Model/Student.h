fbb9719049057ee147be193f1bc94ec08a38e5d2
Devepre <bicycle2life@gmail.com>
2018-04-10 13:03:42 +0300

    Model added

---
+++ KVCO/Model/Student.h
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@property (strong, nonatomic) NSString      *name;
+@property (strong, nonatomic) NSString      *surname;
+@property (strong, nonatomic) NSDate        *dateOfBirth;
+@property (assign, nonatomic) NSInteger     grade;
+
+- (instancetype)initWithName:(NSString *)name andSurname:(NSString *)surname andBirthDate:(NSDate *)birthDate NS_DESIGNATED_INITIALIZER;
+- (NSString *)fullName;
+
+@end

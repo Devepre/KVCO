a530be8f65207bcaef147a77d07d5f6dcee7dbfc
Devepre <bicycle2life@gmail.com>
2018-04-10 12:55:34 +0300

    Initial Commit

---
+++ KVCO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVCO
+//
+//  Created by User on 4/10/18.
+//  Copyright © 2018 DEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
